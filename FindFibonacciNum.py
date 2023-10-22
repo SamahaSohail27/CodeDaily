75c68cff7d07b0b1c433217634fb29e33f015e4c
Samaha Sohail <141258951+SamahaSohail27@users.noreply.github.com>
2023-10-22 18:29:41 +0500

    Add files via upload

---
+++ FindFibonacciNum.py
@@ -0,0 +1,18 @@
+def fib(n):
+    dp = [-1] * (n + 1)
+
+    def helper(n):
+        if n <= 1:
+            return n
+        if dp[n - 1] == -1:
+            dp[n - 1] = helper(n - 1)
+        if dp[n - 2] == -1:
+            dp[n - 2] = helper(n - 2)
+        return dp[n - 1] + dp[n - 2]
+
+    return helper(n)
+
+# Example usage:
+n = 10
+result = fib(n)
+print(result)
