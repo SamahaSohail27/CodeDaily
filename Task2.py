1f7623437b1b3f00df0c77f7fef56c2c3c67daa7
Samaha Sohail <141258951+SamahaSohail27@users.noreply.github.com>
2023-12-20 23:39:38 +0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,11 +1,13 @@
-def countNumOfWays(current_stair,n):
-    if current_stair==n:
-        return 1
-    if current_stair>n:
-        return 0
-    return countNumOfWays(current_stair+1,n)+countNumOfWays(current_stair+2,n)
-
-print(countNumOfWays(0,8))
+def countWays(self, n):       
+        s=n+1
+        dp=[-1]*s       
+        def funct(n):
+            if n<=1:
+                return 1
+            if dp[n] == -1:
+                dp[n]= funct(n-1)+funct(n-2)
+            return dp[n]        
+        return funct(n)
 #4->5
 #5->8
 #8->34
